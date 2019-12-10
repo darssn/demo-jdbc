6fe71162b4c7d41e14572b166b21f1b3be8dc455
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2019-12-10 13:44:15 +0100

    TP 01 jdbc maj 1

--- src/main/java/fr/diginamic/props/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/props/TestConnexionJdbc.java
@@ -3,20 +3,34 @@ package fr.diginamic.props;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		
+		/**Chargement Driver mariadb
+		 * */
 			Class.forName("org.mariadb.jdbc.Driver");
+			
+			/**Recuperation des données du ficher .properties
+			 * */
+			ResourceBundle data = ResourceBundle.getBundle("database");
+			
+			String dbPath = data.getString("db.path");
+			String dbUsername = data.getString("db.username");
+			String dbPwd = data.getString("db.pwd");
 		
-			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/compta", "root", "");
+			/**Ouverture connection a la base de données
+			 * */
+			Connection connect = DriverManager.getConnection(dbPath,dbUsername,dbPwd);
+			
 			
 			System.out.println(connect);
 			
 	
-		
+		/**Fermeture connection a la base de données
+		 * */
 			connect.close();
 		
 	}
